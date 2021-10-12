c7af9bd9540a240ce3f7d15f551dfb714e7ca7c7
default <default8700k@gmail.com>
2021-10-12 11:39:11 +0300

    Added memory cache for Web API.

--- WebApplication/Api/ApiController.cs
+++ WebApplication/Api/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
@@ -14,23 +15,31 @@ namespace WebApplication.Api
 	{
 		private readonly ILogger logger;
 		private readonly IConfiguration configuration;
+		private readonly IMemoryCache memoryCache;
 
-		public ApiController(ILogger<ApiController> logger, IConfiguration configuration)
+		public ApiController(ILogger<ApiController> logger, IConfiguration configuration, IMemoryCache memoryCache)
 		{
 			this.logger = logger;
 			this.configuration = configuration;
+			this.memoryCache = memoryCache;
 		}
 
 		[HttpGet]
 		public IActionResult Main(String json)
 		{
+			if (memoryCache.TryGetValue<Int32>(json, out var cacheResult) == true)
+			{
+				logger.Log(LogLevel.Information, "api:main data from cache: {0}", cacheResult);
+				return new ObjectResult(cacheResult);
+			}
+
 			var limits = new Limit(logger, configuration, "NumberLimits");
 			try
 			{
 				var data = JsonConvert.DeserializeAnonymousType(json, new { values = new List<Int32>() });
 				if (data.values.Count > limits.MaxCount)
 				{
-					logger.Log(LogLevel.Debug, "api:main values.length > limits.MaxCount");
+					logger.Log(LogLevel.Information, "api:main values.length > limits.MaxCount");
 					return StatusCode(415, "count size error");
 				}
 
@@ -39,18 +48,20 @@ namespace WebApplication.Api
 				{
 					if (value < limits.MinValue || value > limits.MaxValue)
 					{
-						logger.Log(LogLevel.Debug, "api:main value < limits.MinValue || value > limits.MaxValue");
+						logger.Log(LogLevel.Information, "api:main value < limits.MinValue || value > limits.MaxValue");
 						return StatusCode(415, "value size error");
 					}
 
 					result += (Int32)Math.Pow(value, 2);
 				}
 
+				memoryCache.Set(json, result,
+					new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(5)));
 				return new ObjectResult(result);
 			}
 			catch (Exception exception)
 			{
-				logger.Log(LogLevel.Error, "{0}", exception.Message);
+				logger.Log(LogLevel.Error, "api:main exception: {0}", exception.Message);
 				return StatusCode(400, "wrong data format");
 			}
 		}
